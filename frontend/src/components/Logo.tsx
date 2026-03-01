f14169775a4d4bd994717570398222ad8b75d782
Ziad Mohsen <zm.ze05ad@gmail.com>
2026-03-02 00:50:45 +0200

    :wrench: refactor: use constant ROUTES.ROOT instead of "/" in Logo.tsx link

--- frontend/src/components/Logo.tsx
+++ frontend/src/components/Logo.tsx
@@ -1,10 +1,11 @@
+import { ROUTES } from "@/constants";
 import { Cloud } from "lucide-react";
 import { Link } from "react-router-dom";
 
 export default function Logo({ icon = false }: { icon?: boolean }) {
   return (
     // TODO: use constant routes enum
-    <Link to="/" className="flex items-center gap-2">
+    <Link to={ROUTES.ROOT} className="flex items-center gap-2">
       <div className="flex items-center justify-center bg-primary text-white p-2 rounded-lg">
         <Cloud />
       </div>
